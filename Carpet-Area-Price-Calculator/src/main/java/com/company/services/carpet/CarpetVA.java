ddbd3a517f38071490b11e940f8c27aa881fce3c
memintan <memintan@gmail.com>
2020-10-31 13:50:37 -0500

    Project-structure-is-created

--- Carpet-Area-Price-Calculator/src/main/java/com/company/services/carpet/CarpetVA.java
+++ Carpet-Area-Price-Calculator/src/main/java/com/company/services/carpet/CarpetVA.java
@@ -2,20 +2,22 @@ package com.company.services.carpet;
 
 import com.company.enums.Cities;
 import com.company.interfaces.carpetPrices.Carpet;
+import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
-public class CarpetTX implements Carpet {
+@Component
+public class CarpetVA implements Carpet {
 
     private static final Map<Cities, BigDecimal> sqPriceForCity = new HashMap<>();
 
     static {
-        sqPriceForCity.put(Cities.DALLAS, new BigDecimal(1.75));
-        sqPriceForCity.put(Cities.AUSTIN, new BigDecimal(1.32));
-        sqPriceForCity.put(Cities.SAN_ANTONIO, new BigDecimal(2.20));
+        sqPriceForCity.put(Cities.MCLEAN, new BigDecimal(4.32));
+        sqPriceForCity.put(Cities.ARLINGTON, new BigDecimal(2.92));
+        sqPriceForCity.put(Cities.FAIRFAX, new BigDecimal(4.65));
 
 
     }
